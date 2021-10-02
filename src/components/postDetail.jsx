d3d8d9947b865032947781894af48992d1d3d303
y-solb <ysolb99@gmail.com>
2021-10-02 16:18:25 +0900

    feat: add redirect

--- src/components/postDetail.jsx
+++ src/components/postDetail.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 
 class PostDetail extends Component {
+  componentDidMount() {
+    const { history, location } = this.props;
+    if (location.state == undefined) {
+      history.push('/');
+    }
+  }
   render() {
-    console.log(this.props.location.state);
-    const { title, body } = this.props.location.state;
-    return (
-      <section className="detail">
-        <div className="detail__box">
-          <p className="detail__title">{title}</p>
-          <p className="detail__body">{body}</p>
-        </div>
-      </section>
-    );
+    const { location } = this.props;
+    if (location.state) {
+      return (
+        <section className="detail">
+          <div className="detail__box">
+            <p className="detail__title">{location.state.title}</p>
+            <p className="detail__body">{location.state.body}</p>
+          </div>
+        </section>
+      );
+    } else {
+      return null;
+    }
   }
 }
 
